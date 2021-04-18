377ffd0bc265d416275703d467be1c83f2837835
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-18 02:41:58 -0300

    Test: add user integration tests

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,53 @@
+import { hash } from "bcryptjs"
+import request from "supertest"
+import { Connection } from "typeorm"
+
+import { v4 as uuid } from "uuid"
+import { app } from "../../../../app"
+import createConnection from "../../../../database/index"
+
+
+let connection: Connection
+
+describe("Authentication", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+    console.log("migration auth")
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+  it("should be able to authenticate session and return user ", async () => {
+    await request(app).post("/api/v1/users/").send({
+      name: "User Test",
+      email: "test1@test.com",
+      password: "admin",
+    });
+
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "test1@test.com",
+      password: "admin"
+    })
+
+    const { token } = response.body
+
+    expect(response.body).toHaveProperty("token")
+    expect(response.body).toHaveProperty("user")
+    expect(response.status).toBe(200)
+
+
+  })
+
+  it("should not be able to authenticate user not exists", async () => {
+    const response = await request(app).post("/api/v1/sessions").send({
+      email: "test@test.com",
+      password: "unkown"
+    })
+
+    expect(response.status).toBe(401)
+  })
+})
---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,56 @@
+import { hash } from "bcryptjs"
+import request from "supertest"
+import { Connection } from "typeorm"
+import { v4 as uuid } from "uuid"
+import { app } from "../../../../app"
+
+import createConnection from "../../../../database/index"
+
+
+
+let connection: Connection
+
+describe("Create User", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+    console.log("migration create")
+
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app).post("/api/v1/users").send({
+      name: "james",
+      email: "aloprado@email.com",
+      password: "1234"
+    })
+
+
+    expect(response.status).toBe(201)
+  })
+  it("should not be able to create a new user if already exists", async () => {
+    const user1 = await request(app).post("/api/v1/users").send({
+      name: "Brian",
+      email: "brian@test.com",
+      password: "1234"
+    })
+
+    const user2 = await request(app).post("/api/v1/users").send({
+      name: "Brian",
+      email: "brian@test.com",
+      password: "1234"
+    })
+
+    expect(user2.status).toBe(400)
+
+
+
+  })
+
+})
